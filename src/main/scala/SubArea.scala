e6b46ef591f4044c8bed63ac6d4bf44c7ef4a7ee
tilda <tilda.toivanen@aalto.fi>
2023-03-05 10:17:20 +0200

    Tein luokan sub-area, joka tällä hetkellä laskee vain kaikki mahdolliset yhdistelmät, muttei vielä filtteröi niitä, joiden numeroita on samalla rivillä tai sarakkeella

---
+++ src/main/scala/SubArea.scala
@@ -0,0 +1,11 @@
+import scala.collection.mutable.Buffer
+
+class SubArea(val summa: Int, squares: Buffer[Int]):
+
+  // At the moment, this method returns every possible combination. First, we make a sequence which consists of
+  // numbers 1,2,3,4,5,6,7,8 and 9. Then, we use Seq's method combinations, which now gives us all the possible
+  // combinations for the cage's (or sub-area's) size. Then, out of these combinations, we use the method filter
+  // to get only the combinations whose sum is equal to the sum of the sub-area. We'll still have to filter the
+  // combinations of numbers which have a number that's in the row or column then.
+  def possibleCombinations: Seq[IndexedSeq[Int]] =
+    (1 to 9).toSeq.combinations(squares.size).toSeq.filter( _.sum == summa)
