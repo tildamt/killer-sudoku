aa44cd70461eb728174d66665fd4cb4cfb71aca6
tilda <tilda.toivanen@aalto.fi>
2023-04-09 18:24:10 +0300

    Kun käyttäjä sulkee ikkunan, niin kesken oleva peli tallennetaan automaattisesti, ja nimeksi tulee DD-MM-YYYY ja kellonaika, minuuttien ja sekuntien kanssa.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,13 @@
+import javafx.event.EventHandler
 import killer.{BigGrid, CreateGrid, filehandling}
 import scalafx.application.JFXApp3
 import scalafx.scene.Scene
+import scalafx.scene.control.{ButtonType, Dialog}
 import scalafx.scene.layout.Pane
+import javafx.stage.WindowEvent
+
+import java.text.DateFormat
+import java.time.{ZoneOffset, ZonedDateTime}
 
 object Main extends JFXApp3 :
 
@@ -14,6 +20,18 @@ object Main extends JFXApp3 :
 
     val gamecreator = new filehandling
     gamecreator.newgame("game1")
-    val creation = new CreateGrid(this, gamecreator.areas1, Array.fill[Option[Int]](9, 9)(None)).create
+    val creator = new CreateGrid(this, gamecreator.areas1, Array.fill[Option[Int]](9, 9)(None))
+    val creation = creator.create
     val scene = creation // Scene acts as a container for the scene graph
     stage.scene = scene // Assigning the new scene as the current scene for the stage
+
+
+    stage.onCloseRequest = (e: WindowEvent) =>
+      val aika = java.util.Calendar.getInstance(java.util.TimeZone.getTimeZone("Europe/Helsinki"))
+      val päivä = aika.get(java.util.Calendar.DAY_OF_MONTH)
+      val vuosi = aika.get(java.util.Calendar.YEAR)
+      val kuukausi = aika.get(java.util.Calendar.MONTH)
+      val tunti = aika.get(java.util.Calendar.HOUR_OF_DAY)
+      val minuutti = aika.get(java.util.Calendar.MINUTE)
+      val sekunti = aika.get(java.util.Calendar.SECOND)
+      gamecreator.paikat(s"${päivä}-${kuukausi}-${vuosi} at ${tunti}-${minuutti}-${sekunti}", creator.theGrid.grid, creator.cages)
