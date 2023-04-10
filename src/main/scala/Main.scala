ade1c019e00ef91c4e5e42f918cc0fdf7efa8ffb
tilda <tilda.toivanen@aalto.fi>
2023-04-10 09:57:16 +0300

    Korjasin bugin, tiedosto ei jostain syystä tallentanut peliä oikein suljettaessa, vaan tallensi aina tyhjän ruudukon. Korjattu vaihtamalla grid-muuttuja arriksi.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import javafx.collections.ObservableList
 import javafx.event.EventHandler
 import killer.{BigGrid, CreateGrid, filehandling}
 import scalafx.application.JFXApp3
@@ -5,6 +6,9 @@ import scalafx.scene.Scene
 import scalafx.scene.control.{ButtonType, Dialog}
 import scalafx.scene.layout.Pane
 import javafx.stage.WindowEvent
+import scalafx.Includes.string2jfxColor
+import scalafx.scene.SceneIncludes.string2jfxColor
+import scalafx.scene.paint.PaintIncludes.string2jfxColor
 
 import java.text.DateFormat
 import java.time.{ZoneOffset, ZonedDateTime}
@@ -18,14 +22,18 @@ object Main extends JFXApp3 :
     stage.width = 850
     stage.title = "Killer Sudoku"
 
+    // New stage is created whose scene is set to a standard, however the user may change it by 
+    // continuing an old game or choosing a new game.
     val gamecreator = new filehandling
-    gamecreator.newgame("game1")
+    gamecreator.newgame("game1")  // standard is game1, but this changeable
     val creator = new CreateGrid(this, gamecreator.areas1, Array.fill[Option[Int]](9, 9)(None))
-    val creation = creator.create
+    val creation = creator.create  // creating a new scene with the class CreateGrid's create method
     val scene = creation // Scene acts as a container for the scene graph
     stage.scene = scene // Assigning the new scene as the current scene for the stage
 
 
+    // When the user closes the app, the current progress is saved to a file which is named 
+    // in the format DD-MM-YYYY at HH-MM-SS
     stage.onCloseRequest = (e: WindowEvent) =>
       val aika = java.util.Calendar.getInstance(java.util.TimeZone.getTimeZone("Europe/Helsinki"))
       val päivä = aika.get(java.util.Calendar.DAY_OF_MONTH)
@@ -34,4 +42,4 @@ object Main extends JFXApp3 :
       val tunti = aika.get(java.util.Calendar.HOUR_OF_DAY)
       val minuutti = aika.get(java.util.Calendar.MINUTE)
       val sekunti = aika.get(java.util.Calendar.SECOND)
-      gamecreator.paikat(s"${päivä}-${kuukausi}-${vuosi} at ${tunti}-${minuutti}-${sekunti}", creator.theGrid.grid, creator.cages)
+      gamecreator.paikat(s"${päivä}-${kuukausi}-${vuosi} at ${tunti}-${minuutti}-${sekunti}", creator.arr, creator.cages)
